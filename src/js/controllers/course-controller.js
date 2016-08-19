36c3323ddbb0d6f28199e54d90fdba8d574d444e
Peixin Liu <liupeixin@wisdomgarden.com>
2016-08-19 17:57:59 +0800

    course detail tab switch

--- src/js/controllers/course-controller.js
+++ src/js/controllers/course-controller.js
@@ -4,10 +4,20 @@ export default ['$scope', '$stateParams',
     ($scope, $stateParams) => {
         $scope.viewName = 'CourseController';
         $stateParams.courseId = parseInt($stateParams.courseId);
+        const TAB_NAMES = ['content', 'outline', 'review'];
+        $scope.ui = {};
+        $scope.ui.tabName = TAB_NAMES[0];
 
         var init = () => {
             $scope.course = $scope.allCourses.find((course) => course.id === $stateParams.courseId);
             $scope.$apply();
+        };
+
+        $scope.onClickTab = (tabName) => {
+            if(TAB_NAMES.indexOf(tabName) == -1) {
+                tabName = 'content'
+            }
+            $scope.ui.tabName = tabName;
         };
 
         $scope.initDataPromise.then(init);
