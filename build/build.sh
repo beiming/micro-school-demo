a3fb5fa8f994a66523d1c3109c88df616206ac17
Peixin Liu <liupeixin@wisdomgarden.com>
2016-08-23 10:47:21 +0800

    build send ENV_TEST param

--- build/build.sh
+++ build/build.sh
@@ -2,26 +2,43 @@
 
 echo 'start building......'
 
+TEST_ENV=$1
+if [ ! -n "$TEST_ENV" ] || [ ! "$TEST_ENV" = true ] && [ ! "$TEST_ENV" = false ];
+    then TEST_ENV=false
+fi
+echo TEST_ENV = $TEST_ENV
+
 version=`cat ./src/data/config.json | grep version`
 version=${version##*: }
 version=${version%%\"}
 version=${version:1}
 echo current version: $version
 
-TEST_ENV=false webpack
+TEST_ENV=$TEST_ENV webpack
 
-echo 'copy data.json'
-./node_modules/json-minify/index.js ./src/data/data.json > ./www/data/data.json
+if [ "$TEST_ENV" != true ]; then
+    echo 'copy data.json'
+    ./node_modules/json-minify/index.js ./src/data/data.json > ./www/data/data.json
 
-echo 'copy config.json'
-./node_modules/json-minify/index.js ./src/data/config.json > ./www/data/config.json
+    echo 'copy config.json'
+    ./node_modules/json-minify/index.js ./src/data/config.json > ./www/data/config.json
+else
+    echo 'copy data.json & config.json'
+    cp ./src/data/* ./www/data
+fi
 
 echo 'copy partials'
 cp -r ./src/partials ./www
 
 node ./build/append_version.js
 
-echo 'packaging......'
-tar -czvf ./dist/micro_school_demo_v${version}.tar.gz ./www/index.html ./www/css ./www/data ./www/icon ./www/js ./www/partials
+if [ "$TEST_ENV" != true ]; then
+    echo 'packaging......'
+    target_file="./dist/micro_school_demo_v${version}.tar.gz"
+    if [ -f "$target_file" ];
+        then rm $target_file
+    fi
 
+    tar -czvf $target_file ./www/index.html ./www/css ./www/data ./www/icon ./www/js ./www/partials
+fi
 echo 'Done'
